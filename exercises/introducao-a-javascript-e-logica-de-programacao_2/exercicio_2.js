dcadb783b3d001c7705970a74ad8aa6e34f00508
FredericoCanoeira <fcanoeira@gmail.com>
2020-11-27 19:30:00 +0100

    resolvido ate o exercicio 7

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/exercicio_2.js
@@ -0,0 +1,8 @@
+//somar todos os valores contidos no array e imprimir o resultado
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for (let index = 0; index < numbers.length; index += 1) {
+    result = numbers[0] + numbers[1] + numbers[2] + numbers[3] + numbers[4]+ numbers[5] + numbers[6] + numbers[7] + numbers[8] + numbers[9];    
+}
+console.log(result);
---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/exercicio_3.js
@@ -0,0 +1,13 @@
+// média aritmética
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
+
+for (let index = 0; index < numbers.length; index += 1) {
+    soma += numbers[index]
+    
+}
+let mediArit = soma/10;
+
+console.log( "A media aritimetica é: " , mediArit);
+
+
